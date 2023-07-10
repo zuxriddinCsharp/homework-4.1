445006e09a907b81f9e54b2b34482b35f1f8eef6
zuxriddinCsharp <137428899+zuxriddinCsharp@users.noreply.github.com>
2023-07-10 15:56:29 +0500

    0 kirgizgunch ketme-ket kelgan sonni tiopish

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int countMax = 0;
+            int a = 1;
+            do
+            {
+                Console.WriteLine("Enter number:");
+                int number = int.Parse(Console.ReadLine());
+                if (number == 0) break;
+                else if (a == number)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (countMax < count)
+                    {
+                        countMax = count;
+                    }
+                    a = number;
+                    count = 1;
+                }
+            }
+            while (true);
+            if (countMax < count)
+            {
+                countMax = count;
+            }
+            Console.WriteLine($"result {countMax} ");
+        }
+    }
+}
